c2d717e7941c8ac2b591e6895290f46b3b7aafdc
luisecc <lcarranza.2890@gmail.com>
2015-10-18 16:45:09 -0500

    2so cambio gulpfile - tareas stylus

--- Gulpfile.js
+++ Gulpfile.js
@@ -14,3 +14,30 @@ gulp.task('server', function() {
         }
     });
 });
+
+
+var stylus = require('gulp-stylus'),
+    nib    = require('nib');
+
+// Pre-procesa archivos Stylus a CSS y recarga los cambios
+gulp.task('css', function() {
+    gulp.src('./app/stylesheets/main.styl')
+        .pipe(stylus({ use: nib() }))
+        .pipe(gulp.dest('./app/stylesheets'))
+        .pipe(connect.reload());
+});
+
+// Recarga el navegador cuando hay cambios en el HTML
+gulp.task('html', function() {
+    gulp.src('./app/**/*.html')
+        .pipe(connect.reload());
+});
+
+// Vigila cambios que se produzcan en el código
+// y lanza las tareas relacionadas
+gulp.task('watch', function() {
+    gulp.watch(['./app/**/*.html'], ['html']);
+    gulp.watch(['./app/stylesheets/**/*.styl'], ['css']);
+});
+
+gulp.task('default', ['server', 'watch']);
