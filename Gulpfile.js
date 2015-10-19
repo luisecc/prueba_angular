2b4736868d800cb5c5342f09f0f30184a17f1ddf
luisecc <lcarranza.2890@gmail.com>
2015-10-19 00:14:01 -0500

    añadiendo cambios

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,21 +1,40 @@
 // File: Gulpfile.js
 'use strict';
 
-var gulp    = require('gulp'),
-    connect = require('gulp-connect'),
-    stylus  = require('gulp-stylus'),
-    nib     = require('nib'),
-    jshint  = require('gulp-jshint'),
-    stylish = require('jshint-stylish'),
+var gulp      = require('gulp'),
+    connect   = require('gulp-connect'),
+    stylus    = require('gulp-stylus'),
+    nib       = require('nib'),
+    jshint    = require('gulp-jshint'),
+    stylish   = require('jshint-stylish'),
+    inject    = require('gulp-inject'),
+    wiredep   = require('wiredep').stream,
+    gulpif    = require('gulp-if'),
+    minifyCss = require('gulp-minify-css'),
+    useref    = require('gulp-useref'),
+    uglify    = require('gulp-uglify'),
+    uncss     = require('gulp-uncss'),
+    angularFilesort = require('gulp-angular-filesort'),
+    templateCache = require('gulp-angular-templatecache'),
     historyApiFallback = require('connect-history-api-fallback');
-
-var inject  = require('gulp-inject');
-var wiredep = require('wiredep').stream;
 
 // Servidor web de desarrollo
 gulp.task('server', function() {
     connect.server({
         root: './app',
+        hostname: '0.0.0.0',
+        port: 8080,
+        livereload: true,
+        middleware: function(connect, opt) {
+            return [ historyApiFallback() ];
+        }
+    });
+});
+
+// Servidor web para probar el entorno de producción
+gulp.task('server-dist', function() {
+    connect.server({
+        root: './dist',
         hostname: '127.0.0.1',
         port: 8080,
         livereload: true,
@@ -25,7 +44,7 @@ gulp.task('server', function() {
     });
 });
 
-// Busca errores en el JS y nos los muestra por pantalla
+// Busca errores en el JS y nos los muestra en el terminal
 gulp.task('jshint', function() {
     return gulp.src('./app/scripts/**/*.js')
         .pipe(jshint('.jshintrc'))
@@ -44,21 +63,28 @@ gulp.task('css', function() {
 // Recarga el navegador cuando hay cambios en el HTML
 gulp.task('html', function() {
     gulp.src('./app/**/*.html')
+        .pipe()
         .pipe(connect.reload());
 });
 
-
-// Busca en las carpetas de estilos y javascript los archivos que hayamos creado
+// Busca en las carpetas de estilos y javascript los archivos
 // para inyectarlos en el index.html
 gulp.task('inject', function() {
-    var sources = gulp.src([ './app/scripts/**/*.js', './app/stylesheets/**/*.css' ]);
-    return gulp.src('index.html', { cwd: './app' })
-        .pipe(inject(sources, {
-            read: false,
-            ignorePath: '/app'
-        }))
+    return gulp.src('index.html', {cwd: './app'})
+        .pipe(inject(
+            gulp.src(['./app/scripts/**/*.js']).pipe(angularFilesort()), {
+                read: false,
+                ignorePath: '/app'
+            }))
+        .pipe(inject(
+            gulp.src(['./app/stylesheets/**/*.css']), {
+                read: false,
+                ignorePath: '/app'
+            }
+        ))
         .pipe(gulp.dest('./app'));
 });
+
 // Inyecta las librerias que instalemos vía Bower
 gulp.task('wiredep', function () {
     gulp.src('./app/index.html')
@@ -68,10 +94,8 @@ gulp.task('wiredep', function () {
         .pipe(gulp.dest('./app'));
 });
 
-
-
-var templateCache = require('gulp-angular-templatecache');
-
+// Compila las plantillas HTML parciales a JavaScript
+// para ser inyectadas por AngularJS y minificar el código
 gulp.task('templates', function() {
     gulp.src('./app/views/**/*.tpl.html')
         .pipe(templateCache({
@@ -82,24 +106,43 @@ gulp.task('templates', function() {
         .pipe(gulp.dest('./app/scripts'));
 });
 
+// Comprime los archivos CSS y JS enlazados en el index.html
+// y los minifica.
+gulp.task('compress', function() {
+    gulp.src('./app/index.html')
+        .pipe(useref.assets())
+        .pipe(gulpif('*.js', uglify({mangle: false })))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(gulp.dest('./dist'));
+});
 
+// Elimina el CSS que no es utilizado para reducir el peso del archivo
+gulp.task('uncss', function() {
+    gulp.src('./dist/css/style.min.css')
+        .pipe(uncss({
+            html: ['./app/index.html', './app/views/post-list.tpl.html', './app/views/post-detail.tpl.html']
+        }))
+        .pipe(gulp.dest('./dist/css'));
+});
 
-
-
-
-
-
-
+// Copia el contenido de los estáticos e index.html al directorio
+// de producción sin tags de comentarios
+gulp.task('copy', function() {
+    gulp.src('./app/index.html')
+        .pipe(useref())
+        .pipe(gulp.dest('./dist'));
+    gulp.src('./app/lib/font-awesome/fonts/**')
+        .pipe(gulp.dest('./dist/fonts'));
+});
 
 // Vigila cambios que se produzcan en el código
 // y lanza las tareas relacionadas
 gulp.task('watch', function() {
-    gulp.watch(['./app/**/*.html'], ['html']);
-
+    gulp.watch(['./app/**/*.html'], ['html', 'templates']);
     gulp.watch(['./app/stylesheets/**/*.styl'], ['css', 'inject']);
     gulp.watch(['./app/scripts/**/*.js', './Gulpfile.js'], ['jshint', 'inject']);
     gulp.watch(['./bower.json'], ['wiredep']);
-
 });
 
-gulp.task('default', ['server', 'inject', 'wiredep', 'watch']);
+gulp.task('default', ['server', 'templates', 'inject', 'wiredep', 'watch']);
+gulp.task('build', ['templates', 'compress', 'copy', 'uncss']);
